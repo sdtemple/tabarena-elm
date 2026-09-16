a304c9864b7eff4cef2f5f239c733a5cebf9f093
Seth Temple <sdtemple@pn2611252.lanl.gov>
2026-09-16 14:12:44 -0600

    WIP: debug unique task ids
    - worried that binary is running with regression
    - script to see which tasks are in the json

---
+++ tabflow_slurm/get_unique_tasks.py
@@ -0,0 +1,22 @@
+# how many unique task ids are in the json file
+
+import numpy as np
+import sys
+
+
+fn = sys.argv[1]
+
+with open(fn, 'r') as f:
+    line = f.readline()
+
+line_split = line.split("task_id")[1:]
+
+keep = []
+for _ in line_split:
+    keep.append(_)
+keep = np.unique(keep)
+
+print(f"There are {len(keep)} unique task IDs")
+print()
+for _ in keep:
+    print(f"Task {_}\n")
